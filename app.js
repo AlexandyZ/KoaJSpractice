8d1737df0a29043b7eaf1d665d02b10d1516185a
Alex Zhang <Alex Zhang>
2017-03-25 20:09:29 -0700

    change structure, make function modules

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-// import koa2. In koa2, we import a class Koa
 const Koa = require('koa');
-
-// create a Koa object as the web app
+const bodyParser = require('koa-bodyparser');
+const controller = require('./controller');
 const app = new Koa();
 
-// app calls the async function to deal with http requests
+// log request URL:
 app.use(async (ctx, next) => {
-    console.log(`${ctx.request.method} ${ctx.request.url}`);
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
-    console.log('finished...');
 });
 
-app.use(async (ctx, next) => {
-    const start = new Date().getTime();
-    await next();
-    const ms = new Date().getTime() - start;
-    console.log('Consumed time:', `${ms}ms`);
-});
+// parse request body:
+app.use(bodyParser());
 
-app.use(async (ctx, next) => {
-    await next(); // call the next async function(middleware)
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
+// add controllers:
+app.use(controller());
 
-// listen port 3000
 app.listen(3000);
 console.log('app started at port 3000...');
